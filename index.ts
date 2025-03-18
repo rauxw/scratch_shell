f12e658ef3b59736b67022d5c7a47ede8f1cd018
rauxw <hardikraut909@gmail.com>
2025-03-18 14:38:41 +0530

    added exit 0

--- index.ts
+++ index.ts
@@ -10,6 +10,8 @@ function runCommand() {
     const answerCommand = answer.trim();
     if (answerCommand.startsWith("echo ")) {
       console.log(answerCommand.slice(5));
+    } else if (answerCommand === "exit 0") {
+      process.exit(0);
     } else {
       console.log(`${answer}: command not found`);
     }
