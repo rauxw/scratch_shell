702a4eaeefe902d8380e051e7ca5ab20ff7ff236
rauxw <hardikraut909@gmail.com>
2025-03-18 14:46:15 +0530

    added buildins

--- index.ts
+++ index.ts
@@ -5,13 +5,21 @@ const rl = createInterface({
   output: process.stdout,
 });
 
+const buildins = ["echo", "exit", "type"];
+
 function runCommand() {
   rl.question("$", (answer) => {
-    const answerCommand = answer.trim();
-    if (answerCommand.startsWith("echo ")) {
-      console.log(answerCommand.slice(5));
-    } else if (answerCommand === "exit 0") {
+    const args = answer.split(" ");
+    if (args[0] === "exit" && args[1] === "0") {
       process.exit(0);
+    } else if (args[0] === "echo") {
+      console.log(args.slice(1).join(" "));
+    } else if (args[0] === "type") {
+      if (buildins.includes(args[1])) {
+        console.log(`${args[1]} is a shell buildin`);
+      } else {
+        console.log(`${args[1]}: not found`);
+      }
     } else {
       console.log(`${answer}: command not found`);
     }
